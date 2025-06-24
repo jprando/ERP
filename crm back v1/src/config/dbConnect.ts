69cb9cb1829190a0a9fb463900a1c0857407e0ee
Jeudi Prando Araujo <jeudiprando@gmail.com>
2025-06-24 10:40:52 -0300

    refact(config\env): validar se DATABASE_URL nao esta vazio

--- crm back v1/src/config/dbConnect.ts
+++ crm back v1/src/config/dbConnect.ts
@@ -1,5 +1,9 @@
-import mongoose from "mongoose";
 import "dotenv/config";
+import mongoose from "mongoose";
+import assert from "node:assert";
+
+assert(process.env.DATABASE_URL, "#ERRO:ENV | O valor de DATABASE_URL nao foi definido!");
+
 class MongooseConnect {
   async databaseConnect(): Promise<mongoose.Connection> {
     mongoose.connect(
